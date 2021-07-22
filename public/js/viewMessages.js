dde9373ef978d2c419055c41d0fa132701582656
Teyon <teyonhherring72@gmail.com>
2021-07-22 18:57:18 +0000

    Locked the user to 3 attempts

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,17 +1,36 @@
+let attempts = 3;
+let run = true;
+passcodeCorrect = false;
+let str = "Test String"
+
+//var SHA256 =  new Hashes.SHA256
+//console.log('SHA256:' + SHA256.hex(str))
 const getMessages = () => {
     const passcode = document.querySelector("#passcode")
     
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val()
-        console.log(data)
-        for (let key in data) {
-            console.log(key, data[key])
-            if(key === passcode.value) {
-                console.log("match found")
-                const message = document.querySelector("#message")
-                message.innerHTML = data[key]
+        //console.log(data)
+        if(run) {    
+            for (let key in data) {
+                //console.log(key, data[key])
+                //message.innerHTML = "failure"
+                if(data[key].passcode === passcode.value) {
+                    console.log("match found")
+                    const message = document.querySelector("#message")
+                    message.innerHTML = data[key].message
+                    passcodeCorrect = true;
+                }
+            }
+            if(!passcodeCorrect && attempts <= 1) {
+                message.innerHTML = "No Attempts Remaining"
+                run = false;
+            }
+            else if(!passcodeCorrect) {
+                message.innerHTML = `Invalid Password, you have ${--attempts} remaining`
             }
         }
+        passcodeCorrect = false;
     })
 }
